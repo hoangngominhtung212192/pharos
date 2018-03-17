93f51a35b96a072d3f7c8016aa3995ab103e3f1d
Tung Hoang Ngo Minh <Tung Hoang Ngo Minh@192.168.1.6>
2018-03-17 20:16:58 +0700

    Add logger to BookDaoImpl

--- PharosApplication/src/main/java/com/pharos/repository/impl/BookDaoImpl.java
+++ PharosApplication/src/main/java/com/pharos/repository/impl/BookDaoImpl.java
@@ -5,6 +5,8 @@ package com.pharos.repository.impl;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Repository;
 
 import com.pharos.entity.Book;
@@ -18,6 +20,8 @@ import com.pharos.repository.BookDao;
 @Repository
 public class BookDaoImpl extends GenericDaoImpl<Book, Integer> implements BookDao {
 
+	private static final Logger LOGGER = LogManager.getLogger(BookDaoImpl.class);
+	
 	/* (non-Javadoc)
 	 * @see com.pharos.repository.BookDao#getAllBooks()
 	 */
