6e338b799af29ea6d33b036abb2c76e0a1ca5c75
Tung Hoang Ngo Minh <Tung Hoang Ngo Minh@192.168.1.6>
2018-03-17 20:03:47 +0700

    Update BookTransformer

--- PharosApplication/src/main/java/com/pharos/transformer/impl/BookTransformerImpl.java
+++ PharosApplication/src/main/java/com/pharos/transformer/impl/BookTransformerImpl.java
@@ -3,8 +3,13 @@
  */
 package com.pharos.transformer.impl;
 
+import java.util.Date;
+
 import com.pharos.dto.BookDTO;
+import com.pharos.entity.Author;
 import com.pharos.entity.Book;
+import com.pharos.entity.Language;
+import com.pharos.entity.Status;
 import com.pharos.transformer.BookTransformer;
 
 /**
@@ -31,10 +36,15 @@ public class BookTransformerImpl implements BookTransformer {
 			dto.setLanguageId(book.getLanguage().getId());
 			dto.setStatusId(book.getStatus().getId());
 			dto.setVoteCount(book.getVoteCount());
+			dto.setAuthorId(book.getAuthor().getId());
+			dto.setPdfLocate(book.getPdf());
+			
+			Date publishedDateUtil = (Date) book.getPublishedDate();
+			dto.setPublishDate(publishedDateUtil);
 			
 		}
 		
-		return null;
+		return dto;
 	}
 
 	/* (non-Javadoc)
@@ -42,8 +52,35 @@ public class BookTransformerImpl implements BookTransformer {
 	 */
 	@Override
 	public Book convertToEntity(BookDTO dto) {
-		// TODO Auto-generated method stub
-		return null;
+		Book book = null;
+		
+		if (dto != null) {
+			book = new Book();
+			book.setTitle(dto.getTiltle());
+			book.setDescription(dto.getDescription());
+			book.setViewCount(dto.getViewCount());
+			book.setVoteCount(dto.getVoteCount());
+			book.setPrice(dto.getPrice());
+			
+			Language language = new Language();
+			language.setId(dto.getLanguageId());
+			
+			book.setLanguage(language);
+			
+			Status status = new Status();
+			status.setId(dto.getStatusId());
+			
+			book.setStatus(status);
+			
+			Author author = new Author();
+			author.setId(dto.getAuthorId());
+			
+			book.setAuthor(author);
+			book.setPdf(dto.getPdfLocate());
+			book.setPublishedDate(new java.sql.Date(dto.getPublishDate().getTime()));
+		}
+		
+		return book;
 	}
 
 }
